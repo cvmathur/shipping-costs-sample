613608d368e3a6c517b39142c4c71e7007cf1a59
cvmathur <cv.mathur@gmail.com>
2017-08-30 22:21:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def makeWebhookResult(req):
         "followupEvent": {
             "name": "charge",
             "data": {
-                "<charges>":"<cost>>"
+                "charges":"cost"
             }
         }
     }
